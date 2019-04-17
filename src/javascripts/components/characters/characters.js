0c54ab5d43df4942e71f73974d550e81100b496c
Keith Walker <Keithrwalker418@gmail.com>
2019-04-16 19:42:47 -0500

    characters printing

---
+++ src/javascripts/components/characters/characters.js
@@ -0,0 +1,36 @@
+import characterData from '../../helpers/data/characterData';
+
+import util from '../../helpers/util';
+
+let characters = [];
+
+const domStringBuilder = () => {
+  let domString = '';
+  characters.forEach((char) => {
+    domString += '<div>';
+    domString += '<div class="img-holder">';
+    domString += `<img src=${char.imageUrl} />`;
+    domString += '</div>';
+    domString += `<h2 class="char-name">${char.name}</h2>`;
+    domString += '</div>';
+  });
+  util.printToDom('characters', domString);
+};
+
+//  This function is calling the function from characterData.js
+//  If it is sucessful in getting the data it runs ".then"
+// If it is unsucessful it runs .catch
+const getData = () => {
+  characterData.getCharacterData()
+    .then((response) => { //  happens when the data is sucessful
+      //  This is assigning the json data to characters[]
+      const charactersArray = response.data.characters;
+      characters = charactersArray;
+      domStringBuilder();
+    })
+    .catch((error) => { //  happens when there is an eeror
+      console.error(error);
+    });
+};
+
+export default { getData };
